bd9b3079cd8207195663390b30b9a806a718198b
Nguyen Trung Kien <kientdn2403@gmail.com>
2022-06-27 11:58:34 +0700

    Http services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-data';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+  getHeroes(): Observable<Hero[]> 
+  {
+    const heroes= of(HEROES)
+    this.messageService.addMessage('HeroService: fetched heroes');
+    return heroes;
+  }
+  getHero(id: number): Observable<Hero>
+  { 
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.addMessage(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
